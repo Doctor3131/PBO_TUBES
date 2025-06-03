3310769dec1c253e0d8fd0233a030ba317907af8
Doctor3131 <sirielwnf@gmail.com>
2025-06-03 10:25:39 +0700

    just a daily commit

--- src/appTerminal/App.java
+++ src/appTerminal/App.java
@@ -1,12 +1,12 @@
 package appTerminal;
 
-import UI.LoginDialog; 
+import UI.login; 
 import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            LoginDialog loginDialog = new LoginDialog(null);
+            login loginDialog = new login();
             loginDialog.setVisible(true);
         });
     }
